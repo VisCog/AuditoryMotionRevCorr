d6150dcf3673136701f949a85fc7c71d17f602d3
Woon Ju Park <woonju.park@gmail.com>
2023-10-16 16:14:42 -0700

    updated paths

--- SetPath.m
+++ SetPath.m
@@ -1,19 +1,25 @@
 
 % run this at the very beginning to add paths
 
-UWtooldir = []; % PATH TO UT TOOLBOX
-funcdir = []; % PATH TO HELPER
-expdir = []; % PATH TO EXPERIMENT
-filterdir = []; % PATH TO SAVED FILTERS
-datadir = []; % PATH TO RAW DATA
-psignifitdir = []; % PATH TO PSIGNIFIT TOOLBOX
+path.local_base = fullfile('~', 'Documents'); 
+path.local_project = fullfile(path.local_base, 'code', 'aud_motion_revcorr', 'personal');
+path.local_tool = fullfile(path.local_base, 'toolbox');
+path.remote_base = fullfile('~', 'Dropbox', 'WoonJuPark', 'Projects'); 
+path.remote_project = fullfile(path.remote_base, 'AuditoryMotionRF');
 
-addpath(UWtooldir)
-addpath(funcdir);
-addpath(expdir);
-addpath(filterdir);
-addpath(datadir);
-addpath(psignifitdir);
+path.UWtool = fullfile(path.local_tool, 'UWToolbox', 'UWToolbox', 'optimization');
+path.func = fullfile(path.local_project, 'func');
+path.exp = fullfile(path.local_project, 'experiment'); 
+path.filter = fullfile(path.remote_project, 'filter');
+path.data = fullfile(path.remote_project, 'data');
+path.psignifit = fullfile(path.local_tool, 'psignifit');
+
+addpath(path.UWtool)
+addpath(path.func);
+addpath(path.exp);
+addpath(path.filter);
+addpath(path.data);
+addpath(path.psignifit);
 
 
 
